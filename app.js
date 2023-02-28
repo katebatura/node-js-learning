4653624d22178ebc09f676ade7fe59415fbd4102
katebatura <katebatura@gmail.com>
2023-02-28 11:43:50 +0100

    signup + login

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const morgan = require('morgan');
 
+// eslint-disable-next-line import/extensions
+const AppError = require('./src/utils/appError');
+const globalErrorHandler = require('./src/controllers/errorController');
+const userRouter = require('./src/routes/userRoutes');
+
 const app = express();
+app.use(express.json());
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Test middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  console.log(req.body);
+  // console.log(req.headers);
+  next();
+});
+
+// 3) ROUTES
+app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
